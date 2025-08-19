a07d0757fc5f0fa0b0478cefc168dbe7e55f9126
Your Name <rajatpandey542@gmail.com>
2025-08-19 18:15:49 +0530

    basic to do list added

--- Objects/object.js
+++ Objects/object.js
@@ -1,36 +1,74 @@
-function Student(name, age, course) {
-    this.name = name;
-    this.age = age;
-    this.course = course;
+// function Student(name, age, course) {
+//     this.name = name;
+//     this.age = age;
+//     this.course = course;
 
-    this.getName=()=> {
-        console.log("name : " + this.name);
-    }
-    this.getAge=()=>{
-        console.log("Age : " + this.age);
-    } 
-    this.getCourse=()=>{
-        console.log("Course : " + this.course);
+//     this.getName=()=> {
+//         console.log("name : " + this.name);
+//     }
+//     this.getAge=()=>{
+//         console.log("Age : " + this.age);
+//     } 
+//     this.getCourse=()=>{
+//         console.log("Course : " + this.course);
+//     }
+
+// }
+
+// class Student{
+//    constructor(name, age, course) {
+//     this.name = name;
+//     this.age = age;
+//     this.course = course; 
+//    };
+
+//    getName=()=> {
+//         console.log("name : " + this.name);
+//     }
+//     getAge=()=>{
+//         console.log("Age : " + this.age);
+//     } 
+//     getCourse=()=>{
+//         console.log("Course : " + this.course);
+//     }
+// }
+
+
+class Todo {
+    constructor(task) {
+        this.task = task;
+        this.completed = false;
     }
 
+    complete() {
+        this.completed = true;
+    }
+
+    display() {
+        console.log(`${this.task} - ${this.completed ? "done" : "Pending"}`);
+    }
 }
 
-class Student{
-   constructor(name, age, course) {
-    this.name = name;
-    this.age = age;
-    this.course = course; 
-   };
-
-   getName=()=> {
-        console.log("name : " + this.name);
+class TodoList{
+    constructor(){
+        this.todos=[];
     }
-    getAge=()=>{
-        console.log("Age : " + this.age);
-    } 
-    getCourse=()=>{
-        console.log("Course : " + this.course);
+
+    addTask(task){
+        const newTodo = new Todo(task);
+        this.todos.push(newTodo);
+    }
+    showAll(){
+        this.todos.forEach((todo)=>todo.display());
     }
 }
 
+// const t1 = new Todo("Reading");
+// t1.display();
+// t1.complete();
+// t1.display();
 
+const list = new TodoList();
+list.addTask("okk","difd","sfsf");
+
+list.showAll();
---
+++ Objects/tempCodeRunnerFile.js
@@ -0,0 +1,17 @@
+class Student{
+//    constructor(name, age, course) {
+//     this.name = name;
+//     this.age = age;
+//     this.course = course; 
+//    };
+
+//    getName=()=> {
+//         console.log("name : " + this.name);
+//     }
+//     getAge=()=>{
+//         console.log("Age : " + this.age);
+//     } 
+//     getCourse=()=>{
+//         console.log("Course : " + this.course);
+//     }
+// }
