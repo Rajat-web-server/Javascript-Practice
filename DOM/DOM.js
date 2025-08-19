83705d48481ba69222032e2e42c458ec8f08977a
Your Name <rajatpandey542@gmail.com>
2025-08-19 23:24:57 +0530

    append and prepend methods added

--- DOM/DOM.js
+++ DOM/DOM.js
@@ -1,6 +1,28 @@
 
-let b1 = document.getElementById("add").addEventListener("click",()=>{
-    const li = document.createElement("button");
+// let b1 = document.getElementById("add");
+// b1.addEventListener("click",(e)=>{
+//     // e.stopPropagation();//only prevents bubbling to ancestors
+//     if(e.target !== b1) return;//this iqnores clicks on child button;
+//     const li = document.createElement("button");
+//     li.innerText="New Item";
+//     b1.append(li);
+// });
+
+let b1 = document.getElementById("add");
+b1.addEventListener("click",(e)=>{
+    // e.stopPropagation();//only prevents bubbling to ancestors
+    if(e.target !== b1) return;//this iqnores clicks on child button;
+    const li = document.createElement("li");
     li.innerText="New Item";
-    add.append(li);
+    b1.appendChild(li);
+});
+
+const b2=document.getElementById("insert");
+b2.addEventListener("click",(e)=>{
+    if(e.target!==b2) return;
+    const li = document.createElement("p");
+    li.textContent="Focus here there's something going on here";
+    li.style.backgroundColor="blue";
+    b2.prepend(li);
 })
+
