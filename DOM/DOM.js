7a084fd643b7d10ba3e244ccfefcd78453603064
Your Name <rajatpandey542@gmail.com>
2025-08-19 22:52:21 +0530

    some changes in the js file of dom.js

--- DOM/DOM.js
+++ DOM/DOM.js
@@ -1,19 +1,6 @@
 
-const list = document.getElementById("list")
-/* const title = document.getElementsById("title")  */
-
-//adding element
-
-document.getElementById("add").addEventListener("click",() => {
-    const li = document.createElement("li");
-    li.textContent="New item (Appended)";
+let b1 = document.getElementById("add").addEventListener("click",()=>{
+    const li = document.createElement("button");
+    li.innerText="New Item";
     add.append(li);
-})
-
-const addd = document.getElementById("replace");
-
-addd.addEventListener("click",()=>{
-    const h1 = document.createElement("h1");
-    h1.textContent = "Hello, World!!";
-    addd.append(h1);
 })
